6fc733169de9b7676d536782106133dcbfda87ff
Anastasi <vashpopo97@mail.ru>
2023-04-08 15:38:26 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int TicketPrice = 95713;
+        int BonusMiles = 20;
+        System.out.println("Количсетво начисленых бонусов: " + TicketPrice / BonusMiles);
     }
 }
